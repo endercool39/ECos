a7a2298c79e49bf10a249b72fa79613b2a0cb012
endercool39 <endercool39@yandex.ru>
2014-08-08 19:53:42 +0400

    Create Methods.java

---
+++ ru.tutorial/Methods.java
@@ -0,0 +1,30 @@
+package ru.tutorial;
+
+import javax.swing.JFrame; // Подключаем класс JFrame из библиотеки java
+
+public class Methods {
+    public String name; // Можно вызвать везде если унаследовать класс
+    private String name2; // Нельзя вызвать 
+    protected String name3; // Можно вызвать , если находитесь в пакете с классом у которого наследуете
+    /*
+    После идефикатора должен идти тип переменной или Имя класса
+    */
+    public JFrame f;
+    private JFrame f2;
+    protected JFrame f3; 
+    
+    public final String NAME = "Значение"; // Правильно
+    public static String nameUser;
+    public final String name222 = ";"; // Неправильно
+    /*
+    После типа или класса , вы должны указать имя объекта чтобы обращаться к полю
+    */
+    public byte nameMethod() // Метод можно будет использовать в другом классе
+    {
+        return 2+2; // Вернет сумму 2+2
+    }
+    byte nameMethod2() // Метод нельзя будет унаследовать
+    {
+        return 2+2; // Вернет сумму 2+2
+    }
+}
