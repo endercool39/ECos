aa5ba0bdb151653928c7656f23fe25b3d4d6084c
endercool39 <endercool39@yandex.ru>
2014-08-08 12:10:56 +0400

    Create Main.java

---
+++ ru.tutorial/Main.java
@@ -0,0 +1,12 @@
+package ru.tutorial; // Имя пакета где находится Main.java
+
+/**
+ *
+ * @author Tutorial
+ */
+public class Main { // Публичный главный класс
+    public static void main(String args[]) // Главный метод который при старте выполнится
+    {
+        // Тут пишем код
+    }
+}
